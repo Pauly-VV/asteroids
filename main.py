8082499f0cdb3fbac0b2ab79389139712ac50ca6
Pauly-VV <pvermeulen26@gmail.com>
2024-09-21 21:44:31 +0200

    added circleshape file and added a frame rate to the screen in main

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #initializing screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    x=0
-    while x < 1:
+    
+    #initialize clock object
+    clock = pygame.time.Clock()
+    #clock timer variable
+    dt = 0
+
+    while True:
+        #enabling quit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #filling black screen and refreshing every loop
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        #limiting framerate
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
