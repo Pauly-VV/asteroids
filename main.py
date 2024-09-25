cbf824972678bab370d7379d9afec0f8f7e318d3
Pauly-VV <pvermeulen26@gmail.com>
2024-09-25 13:19:58 +0200

    added player class and draws player on screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
@@ -14,16 +15,19 @@ def main():
     clock = pygame.time.Clock()
     #clock timer variable
     dt = 0
+    #spawn player
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         #enabling quit button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        #filling black screen and refreshing every loop
+        
+        #filling black screen, drawing player and refreshing every loop
         pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
+        player.draw(screen)
+        pygame.display.flip()        
 
         #limiting framerate
         dt = clock.tick(60) / 1000
